fb3fef22eaa4f83e46d2e442d9a756c3b7f5d36d
Fadoua <ouhadou@rhrk.uni-kl.de>
2022-05-19 14:08:17 +0200

    commit to commit

---
+++ src/applications/helper/encoder_decoder.h
@@ -0,0 +1,15 @@
+#ifndef ENCODER_DECODER_H
+#define ENCODER_DECODER_H
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <stdlib.h>
+#include <vector>
+
+bool kodierer(const uint32_t mysize);
+std::vector<uint8_t> decoded_output();
+
+#endif
+
