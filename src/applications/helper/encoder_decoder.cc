07269dbee3f23ca93987f57a68d180e5e2d23cdb
Fadoua <ouhadou@rhrk.uni-kl.de>
2022-05-27 15:12:08 +0200

    Erstes Step check

--- src/applications/helper/encoder_decoder.cc
+++ src/applications/helper/encoder_decoder.cc
@@ -14,48 +14,27 @@
 
 namespace RaptorQ = RaptorQ__v1;
 
-
 const uint16_t symbol_size = 4; // bytes
 float drop_probability = 20.0;
 const uint8_t overhead = 4;
 std::mt19937_64 rnd;
 RaptorQ::Block_Size block = RaptorQ::Block_Size::Block_10;
 using symbol_id = uint32_t; // just a better name
-//std::vector<std::pair<symbol_id, std::vector<uint8_t>>> received;
-using std::cout; using std::endl;
-const uint32_t mysize = 300; 
-std::vector<uint8_t> output(mysize, 0); 
 
+using std::cout; using std::endl;
+using std::vector;
+using std::make_pair;
+
+std::vector<uint8_t> output(13, 0);
+std::vector<uint8_t> Null;
 
 
 //bool kodierer( std::vector<uint8_t> input, const uint32_t mysize);
 //bool decoder(std::vector<uint8_t> input, const uint32_t mysize);
 
-bool kodierer(const uint32_t mysize)
+bool kodierer( std::vector<uint8_t> input, const uint32_t mysize)
 {   
-    // get a random number generator
-    std::ifstream rand("/dev/urandom");
-    uint64_t seed = 0;
-    rand.read(reinterpret_cast<char *>(&seed), sizeof(seed));
-    rand.close();
-    rnd.seed(seed);
-
-    RaptorQ__v1::local_cache_size(5000000);
     std::vector<std::pair<symbol_id, std::vector<uint8_t>>> received;
-
-    // the actual input.
-    std::vector<uint8_t> input;
-    input.reserve(mysize);
-
-    
-    std::uniform_int_distribution<int16_t> distr(0,
-                                                 std::numeric_limits<uint8_t>::max());
-
-    // fill our input with random data
-    for (size_t idx = 0; idx < mysize; ++idx)
-    {
-        input.push_back(static_cast<uint8_t>(distr(rnd)));
-    }
     auto min_symbols = (input.size() * sizeof(uint8_t)) / symbol_size;
   
 
@@ -172,7 +151,9 @@ bool kodierer(const uint32_t mysize)
                repair_sym_it != enc.end_repair(max_repair);
              ++repair_sym_it)
         {
-           
+            // we save the symbol here:
+            // make sure the vector has enough space for the symbol:
+            // fill it with zeros for the size of the symbol
             std::vector<uint8_t> repair_sym_data(symbol_size, 0);
 
             // save the data of the symbol into our vector
@@ -201,38 +182,68 @@ bool kodierer(const uint32_t mysize)
         }
         if (repair_sym_it == enc.end_repair(enc.max_repair()))
         {
-          
+            // we dropped waaaay too many symbols!
+            // should never happen in real life. it means that we do not
+            // have enough repair symbols.
+            // at this point you can actually start to retransmit the
+            // repair symbols from enc.begin_repair(), but we don't care in
+            // this example
             std::cout << "Maybe losing " << drop_probability << "% is too much?\n";
             return false;
         }
     }
- 
 
+    // Now we all the source and repair symbols are in "received".
+    // we will use those to start decoding:
+
+    // define "Decoder_type" to write less afterwards
     using Decoder_type = RaptorQ::Decoder<
         typename std::vector<uint8_t>::iterator,
         typename std::vector<uint8_t>::iterator>;
     Decoder_type dec(block, symbol_size, Decoder_type::Report::COMPLETE);
-    
-    //std::vector<uint8_t> output(mysize, 0); 
+    // "Decoder_type::Report::COMPLETE" means that the decoder will not
+    // give us any output until we have decoded all the data.
+    // there are modes to extract the data symbol by symbol in an ordered
+    // an unordered fashion, but let's keep this simple.
+
+    // we will store the output of the decoder here:
+    // note: the output need to have at least "mysize" bytes, and
+    // we fill it with zeros
+    //std::vector<uint8_t> output(mysize, 0);
 
     // now push every received symbol into the decoder
     for (auto &rec_sym : received)
     {
-
+        // as a reminder:
+        //  rec_sym.first = symbol_id (uint32_t)
+        //  rec_sym.second = std::vector<uint8_t> symbol_data
         symbol_id tmp_id = rec_sym.first;
         auto it = rec_sym.second.begin();
         auto err = dec.add_symbol(it, rec_sym.second.end(), tmp_id);
         if (err != RaptorQ::Error::NONE && err != RaptorQ::Error::NOT_NEEDED)
         {
-     
+            // When you add a symbol, you can get:
+            //   NONE: no error
+            //   NOT_NEEDED: libRaptorQ ignored it because everything is
+            //              already decoded
+            //   INITIALIZATION: wrong parameters to the decoder contructor
+            //   WRONG_INPUT: not enough data on the symbol?
+            //   some_other_error: errors in the library
             std::cout << "error adding?\n";
             return false;
         }
     }
 
-    
+    // by now we now there will be no more input, so we tell this to the
+    // decoder. You can skip this call, but if the decoder does not have
+    // enough data it sill wait forever (or until you call .stop())
     dec.end_of_input(RaptorQ::Fill_With_Zeros::NO);
-    
+    // optional if you want partial decoding without using the repair
+    // symbols
+    // std::vector<bool> symbols_bitmask = dec.end_of_input (
+    //
+
+    // decode, and do not return until the computation is finished.
     auto res = dec.wait_sync();
     if (res.error != RaptorQ::Error::NONE)
     {
@@ -246,7 +257,12 @@ bool kodierer(const uint32_t mysize)
     auto out_it = output.begin();
     auto decoded = dec.decode_bytes(out_it, output.end(), decode_from_byte,
                                     skip_bytes_at_begining_of_output);
-
+    // "decode_from_byte" can be used to have only a part of the output.
+    // it can be used in advanced setups where you ask only a part
+    // of the block at a time.
+    // "skip_bytes_at_begining_of_output" is used when dealing with containers
+    // which size does not align with the output. For really advanced usage only
+    // Both should be zero for most setups.
 
     if (decoded.written != mysize)
     {
@@ -268,7 +284,7 @@ bool kodierer(const uint32_t mysize)
     {
         std::cout << "Decoded: " << mysize << "\n";
     }
-
+    // byte-wise check: did we actually decode everything the right way?
     for (uint64_t i = 0; i < mysize; ++i)
     {
         if (input[i] != output[i])
@@ -280,11 +296,23 @@ bool kodierer(const uint32_t mysize)
     }
 
     return true;
-} 
+}
 std::vector<uint8_t> decoded_output()
 { 
-    return output; 
-} 
-
-
-
+   return output; 
+}
+/*std::vector<std::vector<uint8_t>> fct()
+{
+	std::vector<uint8_t> symbol_data;
+	std::vector<std::vector<uint8_t>>items;
+	//std::cout<< "received"<<received.size()<<"\n";
+	for (auto &rec_sym : received)
+	{
+		// rec_sym.first = symbol_id;
+		rec_sym.second = symbol_data;
+		items.push_back(symbol_data);
+	
+    }
+	return items;
+}
+*/
